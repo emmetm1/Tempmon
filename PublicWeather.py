60ca60656838f3290a3d3724bf9aa22a4871bc95
emmetm1 <emmetm1@gmail.com>
2022-03-24 10:58:36 -0500

    Worked on public weather. Still needs work

--- PublicWeather.py
+++ PublicWeather.py
@@ -1,15 +1,51 @@
+import traceback
+import logging
+import time
 from pyowm import OWM
 from pyowm.utils import config
 from pyowm.utils import timestamps
+from datetime import datetime
+import influxdb_client
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
 
-# ---------- FREE API KEY examples ---------------------
 
-owm = OWM('API Key here')
+# Initilize owm
+owm = OWM('cc98066d355099341267dd7559c5f6b0')
 mgr = owm.weather_manager()
 
+# Initilize Influx Client
+token = "Gtvnbd9_wl5bf52mPfXCVc-ERnbEGMMTRscK_fhwQqnoL1V58sJpsjW42gDhQh2ANIV_GCZoTsAcMXV5pLSm4A=="
+org = "whem-home"
+bucket = "Weather-Data"
+client = influxdb_client.InfluxDBClient(url="http://192.168.50.66:8086", token=token, org=org)
+write_api = client.write_api(write_options=SYNCHRONOUS)
 
-# Search for current weather in Minneapolis and get details
 
-weather = mgr.weather_at_place('Minneapolis,US').weather
-temp_dict_fahrenheit = weather.temperature('fahrenheit')  # a dict in Fahrenheit units
-print("The temp is" + str(temp_dict_fahrenheit))
+
+# Test prints to confirm values
+#print("temp is " + str(tempf))
+#print("humidity is " + str(humid))
+
+
+while True:
+    try:
+        # Search for current weather in Minneapolis and get details
+        one_call = mgr.one_call(lat=44.980, lon=-93.264, exclude='minutely,hourly', units='imperial')
+
+        # Set weather values
+        humidity = one_call.current.humidity 
+        tempdic = one_call.current.temperature()
+        tempf = tempdic["temp"]
+
+        # Write to Influx 
+        TempData = Point("Temp").tag("location", "Outdoors").field("temp_f", tempf).time(datetime.utcnow(), WritePrecision.NS)
+        HumidityData = Point("Humidity").tag("location", "Outdoors").field("humidity", humidity).time(datetime.utcnow(), WritePrecision.NS)
+        write_api.write(bucket, org, TempData)
+        write_api.write(bucket, org, HumidityData)
+        print("write to db success", temp_f)
+        time.sleep(10)
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        exit
+        # Logs the error appropriately. 
