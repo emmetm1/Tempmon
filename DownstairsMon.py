086d190bc99e8b7fee5d12836ae311d4399dd6f6
emmetm1 <emmetm1@gmail.com>
2023-01-25 16:23:19 -0600

    added improvments from last commit to downstairs

--- DownstairsMon.py
+++ DownstairsMon.py
@@ -7,6 +7,10 @@ import board
 import time
 import os
 from dotenv import load_dotenv
+import logging
+
+log_file = '/var/log/tempmon.log'
+logging.basicConfig(filename=log_file, level=logging.DEBUG)
 
 # load env variables
 load_dotenv()
@@ -28,12 +32,18 @@ while True:
         humidity = dht.humidity
         temp_f = temp_c * 9.0 / 5.0 + 32.0
 
+        if humidity is None or temp_c is None:
+            raise Exception("Failed to read from DHT22 sensor")
+        if humidity > 100 or humidity < 0 or temp_f < -40 or temp_f > 120:
+            raise Exception("Invalid data received from DHT22 sensor")
+
         TempData = Point("Temp").tag("location", location).field("temp_f", temp_f).time(datetime.utcnow(), WritePrecision.NS)
         HumidityData = Point("Humidity").tag("location", location).field("humidity", humidity).time(datetime.utcnow(), WritePrecision.NS)
         write_api.write(bucket, org, TempData)
         write_api.write(bucket, org, HumidityData)
         
-        print("write to db success", temp_f)
+        logging.info("write to db success. temp_f %s, humidity %s", temp_f, humidity)
         time.sleep(300)
-    except RuntimeError as e:
-        continue
+    except Exception as e:
+        logging.error("Error while reading from sensor: %s", e)
+        time.sleep(10)
